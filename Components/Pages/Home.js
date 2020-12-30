b12727f49c469ae94d1e2faf5aae117c4b500527
ChrisCruze <cruzc09@gmail.com>
2020-12-30 09:51:00 -0500

    added expo push notifications

--- Components/Pages/Home.js
+++ Components/Pages/Home.js
@@ -1,12 +1,26 @@
 import { StatusBar } from "expo-status-bar";
-import React, { useState, useEffect, Fragment } from "react";
+import React, { useState, useEffect, Fragment, useRef } from "react";
+import Constants from "expo-constants";
+import * as Notifications from "expo-notifications";
+import * as Permissions from "expo-permissions";
 import { StyleSheet, Text, View } from "react-native";
 import { Button, ThemeProvider, Header, Input, CheckBox } from "react-native-elements";
 import { firebase, firebase_sign_out } from "../../firebase";
-
+Notifications.setNotificationHandler({
+  handleNotification: async () => ({
+    shouldShowAlert: true,
+    shouldPlaySound: false,
+    shouldSetBadge: false
+  })
+});
 function Home({ navigation }) {
   const [phoneNumber, updatePhoneNumber] = useState("");
   const [checkBoxStateQuote, updateCheckBoxStateQuote] = useState(false);
+
+  const [expoPushToken, setExpoPushToken] = useState("");
+  const [notification, setNotification] = useState(false);
+  const notificationListener = useRef();
+  const responseListener = useRef();
 
   const firebaseRef = firebase
     .database()
@@ -32,6 +46,27 @@ function Home({ navigation }) {
     firebaseRef.update({ phoneNumber: phoneNumber });
   };
 
+  useEffect(() => {
+    registerForPushNotificationsAsync().then(token => {
+      setExpoPushToken(token);
+      firebaseRef.update({ ExpoPushToken: token });
+    });
+
+    // This listener is fired whenever a notification is received while the app is foregrounded
+    notificationListener.current = Notifications.addNotificationReceivedListener(notification => {
+      setNotification(notification);
+    });
+
+    // This listener is fired whenever a user taps on or interacts with a notification (works when app is foregrounded, backgrounded, or killed)
+    responseListener.current = Notifications.addNotificationResponseReceivedListener(response => {
+      console.log(response);
+    });
+
+    return () => {
+      Notifications.removeNotificationSubscription(notificationListener);
+      Notifications.removeNotificationSubscription(responseListener);
+    };
+  }, []);
   return (
     <View style={styles.container}>
       <Text>Enter Your Phone Number to Receive Bot Alerts</Text>
@@ -49,6 +84,36 @@ function Home({ navigation }) {
       <StatusBar style="auto" />
     </View>
   );
+}
+async function registerForPushNotificationsAsync() {
+  let token;
+  if (Constants.isDevice) {
+    const { status: existingStatus } = await Permissions.getAsync(Permissions.NOTIFICATIONS);
+    let finalStatus = existingStatus;
+    if (existingStatus !== "granted") {
+      const { status } = await Permissions.askAsync(Permissions.NOTIFICATIONS);
+      finalStatus = status;
+    }
+    if (finalStatus !== "granted") {
+      alert("Failed to get push token for push notification!");
+      return;
+    }
+    token = (await Notifications.getExpoPushTokenAsync()).data;
+    console.log(token);
+  } else {
+    alert("Must use physical device for Push Notifications");
+  }
+
+  if (Platform.OS === "android") {
+    Notifications.setNotificationChannelAsync("default", {
+      name: "default",
+      importance: Notifications.AndroidImportance.MAX,
+      vibrationPattern: [0, 250, 250, 250],
+      lightColor: "#FF231F7C"
+    });
+  }
+
+  return token;
 }
 
 export default Home;
